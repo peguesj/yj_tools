61bd94110b1e9cc47276e4d27a0f22c341f80f14
Jeremiah Pegues <jeremiah.pegues@gmail.com>
2023-05-26 14:45:35 -0400

    Hybrid Demucs has entered the chat

--- remove_vocals.py
+++ remove_vocals.py
@@ -1,33 +1,42 @@
-import sys
+import argparse
 import os
+import subprocess
 from spleeter.separator import Separator
-from pydub import AudioSegment
 
-def remove_vocals(input_file):
-    # Define separator
-    separator = Separator('spleeter:2stems')
+def remove_vocals(input_path, use_spleeter):
+    # Get the base path, filename, and extension
+    base_path, filename = os.path.split(input_path)
+    file_base, file_ext = os.path.splitext(filename)
 
-    # Separate audio
-    output_folder = 'spleeter_output'
-    separator.separate_to_file(input_file, output_folder)
+    # Construct the output path
+    output_path = os.path.join(base_path, f"{file_base}_NoVocals{file_ext}")
 
-    # The output is in a sub-folder named after the input file (without extension)
-    base_file_name = os.path.basename(input_file)
-    base_name_no_ext = os.path.splitext(base_file_name)[0]
-    output_subfolder = os.path.join(output_folder, base_name_no_ext)
+    # Depending on the flag, use either Demucs or Spleeter
+    if use_spleeter:
+        separator = Separator('spleeter:2stems')
+        separator.separate_to_file(input_path, base_path, filename_format='{filename}/{instrument}{extension}')
+        
+        # The vocals will be in a file named {file_base}/vocals{file_ext}, remove it
+        os.remove(os.path.join(base_path, file_base, 'vocals' + file_ext))
+        
+        # Rename the accompaniment to the desired output filename
+        os.rename(os.path.join(base_path, file_base, 'accompaniment' + file_ext), output_path)
+        
+        # Remove the created directory
+        os.rmdir(os.path.join(base_path, file_base))
+    else:
+        # Use Demucs
+        subprocess.run(['demucs', '-n', 'htdemucs_ft', '--two-stems=other', input_path])
 
-    # The accompaniment is saved in a file named "accompaniment.wav"
-    output_file = os.path.join(output_subfolder, 'accompaniment.wav')
-
-    # Convert the output file to the same format as the input file
-    output_audio = AudioSegment.from_wav(output_file)
-    final_output_file = os.path.splitext(input_file)[0] + '_NoVocals' + os.path.splitext(input_file)[1]
-    output_audio.export(final_output_file, format=os.path.splitext(input_file)[1].replace('.', ''))
-
-    print(f'Vocal removed audio file has been saved as: {final_output_file}')
-
+        # The other track will be in a directory named separated/htdemucs_ft/{file_base}/{file_base}_other.wav
+        # Move it to the desired output filename
+        os.rename(os.path.join('separated', 'htdemucs_ft', file_base, f'{file_base}_other.wav'), output_path)
 
 if __name__ == "__main__":
-    input_file = sys.argv[1]
-    remove_vocals(input_file)
+    parser = argparse.ArgumentParser(description="Remove vocals from an audio file.")
+    parser.add_argument('input_path', type=str, help="The path to the audio file.")
+    parser.add_argument('-alt', action='store_true', help="Use Spleeter instead of Hybrid Demucs.")
 
+    args = parser.parse_args()
+
+    remove_vocals(args.input_path, args.alt)
